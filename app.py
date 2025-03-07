4963fdd8460f89b9fcae219983b6eb470846657d
paufer2337 <paufer2337@gmail.com>
2025-03-07 15:39:14 +0100

    templates added

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import render_template
 
 app = Flask(__name__)
-db = SQLAlchemy()
+#db = SQLAlchemy(app)
 
 
 
 app.route('/')
+def index():
+    return render_template('templates/index.html')
+
+
+
+app.run(debug=True)
